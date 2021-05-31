effba2ce60e2d5c78c13dd06b73fb0e8852a6791
srinivasthanneeru <srinivast@gmail.com>
2021-05-31 15:12:25 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,10 @@ node 'master.puppet.vm' {
   content => "Welcome to ${fqdn}\n",
  }
 }
+node 'minecraft.puppet.vm' {
+   include role::minecraft_server
+}
+
 node /^web/ {
   include role::app_server
 }
