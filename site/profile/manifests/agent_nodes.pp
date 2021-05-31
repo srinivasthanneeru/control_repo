ac37abbd910606b9fca51ce81cc4c07b7874075a
srinivasthanneeru <srinivast@gmail.com>
2021-05-31 14:06:27 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -5,10 +5,10 @@ class profile::agent_nodes {
  
  host {'web.puppet.vm':
   ensure => present,
-  ip     => 172.18.0.2,
+  ip     => '172.18.0.2',
  }
  host {'db.puppet.vm':
   ensure => present,
-  ip     => 172.18.0.3,
+  ip     => '172.18.0.3',
  }
 }
