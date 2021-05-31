30fc6a6349a1ef150e52da08c447883cf9895053
srinivasthanneeru <srinivast@gmail.com>
2021-05-31 15:07:30 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -18,4 +18,8 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
 }
